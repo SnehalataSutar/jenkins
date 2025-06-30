e4c7353699f9ced9f3f6142585e48e0d33dd8094
gauri <snehalatasutar1204@gmail.com>
2025-07-01 00:18:26 +0530

    Pipeline file added

---
+++ mypipeline.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent {
+        label 'node1'
+    }
+    stages {
+        stage('pull') {
+            steps {
+                echo "Pulling Successful"
+            }
+        }
+        stage('building') {
+            steps {
+                echo "Building Successful"
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "Testing Successful"
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "Deploy Successful"
+            }
+        }
+    }
