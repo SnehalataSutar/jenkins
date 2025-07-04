fd3eb63217fe536b7059ca0fae7edf11914f15f7
gauri <snehalatasutar1204@gmail.com>
2025-07-04 15:32:18 +0530

    --- Auto Git Commit ---

--- mypipeline.groovy
+++ mypipeline.groovy
@@ -19,7 +19,7 @@ pipeline {
             steps {
                 withSonarQubeEnv(installationName: 'sonar-server', credentialsId: 'sonar-token') {
                  sh'''  
-                    
+                    PATH=
                     mvn clean verify sonar:sonar \
                     -Dsonar.projectKey=Student \
                     -Dsonar.projectName='Student'
